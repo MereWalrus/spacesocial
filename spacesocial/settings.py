a5e67fe956b7dd3b324e1e64d892bfed211a25bf
eugenezhavoronok@mail.ru <eugenezhavoronok@mail.ru>
2021-07-09 16:27:35 +0300

    Add navigation and account management

--- spacesocial/settings.py
+++ spacesocial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
@@ -126,3 +127,6 @@ STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
