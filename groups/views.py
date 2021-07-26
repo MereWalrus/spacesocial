b1deea9ddd9ca86558270182f8692855cd09107c
eugenezhavoronok@mail.ru <eugenezhavoronok@mail.ru>
2021-07-26 21:48:06 +0300

    Add LeaveGroup view

--- groups/views.py
+++ groups/views.py
@@ -35,3 +35,20 @@ class JoinGroup(LoginRequiredMixin,generic.RedirectView):
             messages.success(self.request,'You are now a member')
 
         return super().get(request,*args,**kwargs)
+
+class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
+    def get_redirect_url(self,*args,**kwargs):
+        return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self,request,*args,**kwargs):
+        try:
+            membership = GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except GroupMember.DoesNotExist:
+            messages.warning(self.request,'Sorry you are not in this group!')
+        else:
+            membership.delete()
+            messages.success(self.request,'You have left the group!')
+        return super().get(request,*args,**kwargs)
